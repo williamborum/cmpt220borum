cf7507904dea8ae97d48a5749f727f585ad8fe0a
Juan Arias <juan.arias@marist.edu>
2017-02-05 10:25:32 -0500

    Feedback for lab 2

--- labs/2/Ex4_13Borum.java
+++ labs/2/Ex4_13Borum.java
@@ -22,7 +22,21 @@ public class Ex4_13Borum {
     
     //Consonant unless the char is one of A,E,I,O,U,a,e,i,o,u
     String charType = "a consonant";
-    if(t % 26 == 0 || t % 26 == 4 || t % 26 == 8 || t % 26 == 14 || t % 26 == 20)
+   // JA: You don't need the % operator. You are only checking for 5 of the vowels. 
+   // Another option would be to use a switch statement:
+   // switch (s) {
+   //   case 'A':
+   //   case 'E':
+   //   case 'I':
+   //   case 'O':
+   //   case 'U':
+   //   case 'a':
+   //   case 'e':
+   //   case 'i':
+   //   case 'o':
+   //   case 'u': charType = "a vowel"; break;
+   //   case default: ...
+    if(t % 26 == 0 || t % 26 == 4 || t % 26 == 8 || t % 26 == 14 || t % 26 == 20) 
       charType = "a vowel";
     
     if(t < 0 || t > 51)

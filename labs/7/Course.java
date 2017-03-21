fa00cb210397649ede37f913f1746fdaded430dc
Will Borum <william.borum1@marist.edu>
2017-03-21 19:01:47 -0400

    Add comments

--- labs/7/Course.java
+++ labs/7/Course.java
@@ -7,10 +7,12 @@ public class Course {
   private String[] students = new String[100];
   private int numberOfStudents;
   
+  //Create course
   public Course(String courseName) {
     this.courseName = courseName;
   }
   
+  //Add student to course
   public void addStudent(String student) {
     if (numberOfStudents >= students.length) {
       String[] temp = new String[students.length * 2];
@@ -20,18 +22,23 @@ public class Course {
     
     students[numberOfStudents++] = student;
   }
+  
+  //Get the list of students
   public String[] getStudents() {
     return students;
   }
   
+  //Get the number of students
   public int getNumberOfStudents() {
     return numberOfStudents;
   }
   
+  //Get the course name
   public String getCourseName() {
     return courseName;
   }
   
+  //Drop a student from course
   public void dropStudent(String student) {
     String[] temp = new String[students.length];
     int y = 0;
@@ -45,11 +52,13 @@ public class Course {
     System.arraycopy(temp, 0, students, 0, students.length);
   }
   
+  //Drop all students from course
   public void clear() {
     String[] temp = new String[students.length];
     System.arraycopy(temp, 0, students, 0, students.length);
   }
   
+  //Returns a nicely formatted list of the students in course
   public String toString() {
     String t = "[";
     for(String i : students) {
@@ -61,6 +70,7 @@ public class Course {
   
   
   public static void main(String[] args) {
+    //Display the results
     Course c = new Course("Math");
     c.addStudent("Tim");
     c.addStudent("Mary");

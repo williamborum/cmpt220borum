c8cb893dcb990186b942c20a25492800db028ba3
Will Borum <william.borum1@marist.edu>
2017-03-21 19:14:59 -0400

    Add comments

--- labs/7/Divisible.java
+++ labs/7/Divisible.java
@@ -5,6 +5,8 @@ import java.math.BigInteger;
 
 public class Divisible {
   
+  //Returns a list of size 10 of BigIntegers that are bigger than Long.MAX_VALUE
+  //and are divisible by either x or y
   public static BigInteger[] divBy(int x, int y) {
     int z = 0;
     BigInteger a = new BigInteger("9223372036854775807");
@@ -14,8 +16,10 @@ public class Divisible {
     BigInteger e = new BigInteger("0");
     BigInteger[] tmp = new BigInteger[10];
     
+    //Step through until the list has 10 entries
     while(z<10) {
       a = a.add(b);
+      //Check if the number is divisible by either x or y and add to the list
       if(a.divideAndRemainder(c)[1].equals(e)) tmp[z] = a;
       else if(a.divideAndRemainder(d)[1].equals(e)) tmp[z] = a;
       else z--;
@@ -24,6 +28,7 @@ public class Divisible {
     return tmp;
   }
   
+  //Returns a nicely formatted list of a given list of BigIntegers
   public static String toStrings(BigInteger[] tmp) {
     String s = "[";
     for(BigInteger num : tmp) {
@@ -36,6 +41,7 @@ public class Divisible {
   
   
   public static void main(String[] args) {
+    //Display the results
     System.out.println("The first ten numbers greater than Long.MAX_VALUE are " + toStrings(divBy(5, 6)));
   }
 }
